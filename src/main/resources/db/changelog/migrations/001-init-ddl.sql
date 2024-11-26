b63d6192e587eca05016d68548fda7e1093552d9
Багров Матвей <matvey.bagr77@mail.ru>
2024-11-26 12:23:49 +0300

    Обновил 001-init-ddl.sql, добавил таблицу проекта

--- src/main/resources/db/changelog/migrations/001-init-ddl.sql
+++ src/main/resources/db/changelog/migrations/001-init-ddl.sql
@@ -3,4 +3,27 @@ CREATE TABLE IF NOT EXISTS question
     id            BIGSERIAL NOT NULL PRIMARY KEY,
     question_text TEXT      NOT NULL UNIQUE,
     answer        TEXT
-)
+);
+
+CREATE TABLE IF NOT EXISTS project (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(255),
+    direction VARCHAR(255),
+    type VARCHAR(255),
+    min_members INT,
+    max_members INT,
+    goal VARCHAR(1000),
+    description VARCHAR(1000),
+    materials VARCHAR(1000),
+    selling_description VARCHAR(1000),
+    algorithm VARCHAR(1000),
+    competencies VARCHAR(1000),
+    recommendations VARCHAR(1000),
+    complexity VARCHAR(1000),
+    study_format VARCHAR(1000),
+    intense VARCHAR(1000),
+    certificate_conditions VARCHAR(1000),
+    expected_result VARCHAR(1000),
+    grading_criteria VARCHAR(1000),
+    benefits VARCHAR(1000)
+);
