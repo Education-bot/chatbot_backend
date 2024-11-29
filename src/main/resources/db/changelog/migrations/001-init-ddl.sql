fb823c00280b7057cab7e529a185fa52e8542184
romankuryshev <romankuryshev1406@gmail.com>
2024-11-29 21:55:37 +0300

    hotfix

--- src/main/resources/db/changelog/migrations/001-init-ddl.sql
+++ src/main/resources/db/changelog/migrations/001-init-ddl.sql
@@ -5,25 +5,26 @@ CREATE TABLE IF NOT EXISTS question
     answer        TEXT
 );
 
-CREATE TABLE IF NOT EXISTS project (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(255),
-    direction VARCHAR(255),
-    type VARCHAR(255),
-    min_members INT,
-    max_members INT,
-    goal VARCHAR(1000),
-    description VARCHAR(1000),
-    materials VARCHAR(1000),
-    selling_description VARCHAR(1000),
-    algorithm VARCHAR(1000),
-    competencies VARCHAR(1000),
-    recommendations VARCHAR(1000),
-    complexity VARCHAR(1000),
-    study_format VARCHAR(1000),
-    intense VARCHAR(1000),
-    certificate_conditions VARCHAR(1000),
-    expected_result VARCHAR(1000),
-    grading_criteria VARCHAR(1000),
-    benefits VARCHAR(1000)
+CREATE TABLE IF NOT EXISTS project
+(
+    id                     BIGSERIAL NOT NULL PRIMARY KEY,
+    name                   TEXT,
+    direction              TEXT,
+    type                   TEXT,
+    min_members            INT,
+    max_members            INT,
+    goal                   TEXT,
+    description            TEXT,
+    materials              TEXT,
+    selling_description    TEXT,
+    algorithm              TEXT,
+    competencies           TEXT,
+    recommendations        TEXT,
+    complexity             TEXT,
+    study_format           TEXT,
+    intense                TEXT,
+    certificate_conditions TEXT,
+    expected_result        TEXT,
+    grading_criteria       TEXT,
+    benefits               TEXT
 );
