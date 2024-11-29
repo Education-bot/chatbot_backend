c1ba7264fc5200e6dcf565b12ad0462387a893da
romankuryshev <romankuryshev1406@gmail.com>
2024-11-29 22:10:50 +0300

    hotfix2

--- src/main/resources/db/changelog/migrations/001-init-ddl.sql
+++ src/main/resources/db/changelog/migrations/001-init-ddl.sql
@@ -4,27 +4,3 @@ CREATE TABLE IF NOT EXISTS question
     question_text TEXT      NOT NULL UNIQUE,
     answer        TEXT
 );
-
-CREATE TABLE IF NOT EXISTS project
-(
-    id                     BIGSERIAL NOT NULL PRIMARY KEY,
-    name                   TEXT,
-    direction              TEXT,
-    type                   TEXT,
-    min_members            INT,
-    max_members            INT,
-    goal                   TEXT,
-    description            TEXT,
-    materials              TEXT,
-    selling_description    TEXT,
-    algorithm              TEXT,
-    competencies           TEXT,
-    recommendations        TEXT,
-    complexity             TEXT,
-    study_format           TEXT,
-    intense                TEXT,
-    certificate_conditions TEXT,
-    expected_result        TEXT,
-    grading_criteria       TEXT,
-    benefits               TEXT
-);
---
+++ src/main/resources/db/changelog/migrations/002-create-project-table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS project
+(
+    id                     BIGSERIAL NOT NULL PRIMARY KEY,
+    name                   TEXT,
+    direction              TEXT,
+    type                   TEXT,
+    min_members            INT,
+    max_members            INT,
+    goal                   TEXT,
+    description            TEXT,
+    materials              TEXT,
+    selling_description    TEXT,
+    algorithm              TEXT,
+    competencies           TEXT,
+    recommendations        TEXT,
+    complexity             TEXT,
+    study_format           TEXT,
+    intense                TEXT,
+    certificate_conditions TEXT,
+    expected_result        TEXT,
+    grading_criteria       TEXT,
+    benefits               TEXT
+);
