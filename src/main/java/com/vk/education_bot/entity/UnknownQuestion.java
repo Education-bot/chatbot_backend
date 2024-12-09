b3c21d8ffe1e5092c1ca8d37eddfc23a84f9f4f5
Matvey Bagrov <matvey.bagr77@mail.ru>
2024-12-10 00:27:28 +0300

    feat #46: добавлена обработка неизвестных вопросов + 3 команды админа

--- src/main/java/com/vk/education_bot/entity/UnknownQuestion.java
+++ src/main/java/com/vk/education_bot/entity/UnknownQuestion.java
@@ -12,20 +12,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "unknown_question")
 @Getter
 @Setter
-@Table(name = "question")
 @RequiredArgsConstructor
 @AllArgsConstructor
-public class Question {
+public class UnknownQuestion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "question_text")
-    private String text;
+    @Column(name = "user_id", nullable = false)
+    private long userId;
 
-    @Column(name = "answer")
-    private String answer;
+    @Column(name = "question_text", nullable = false, unique = true)
+    private String questionText;
+
+    public UnknownQuestion(long userId, String questionText) {
+        this.userId = userId;
+        this.questionText = questionText;
+    }
 }
