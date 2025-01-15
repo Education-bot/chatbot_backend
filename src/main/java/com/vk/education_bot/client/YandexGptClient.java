1570927c1b5d9a77cbb0723ed0e51dce1307f7f9
Roman <romankuryshev1406@gmail.com>
2025-01-15 20:18:58 +0300

    feat-52: add all questions support (#61)
    * feat-52: add all questions support
    
    * feat-52: fix ddl generation
    
    * feat-52: add all questions, ratelimiter, random answers
    
    * feat-52: minor fixes

--- src/main/java/com/vk/education_bot/client/YandexGptClient.java
+++ src/main/java/com/vk/education_bot/client/YandexGptClient.java
@@ -5,14 +5,24 @@ import com.vk.education_bot.dto.question.ProjectQuestionGptRequest;
 import com.vk.education_bot.dto.question.ProjectQuestionGptResponse;
 import com.vk.education_bot.dto.question.QuestionPrediction;
 import com.vk.education_bot.dto.question.QuestionPredictionRequest;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.reactor.ratelimiter.operator.RateLimiterOperator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.List;
 
+/**
+ * todo rewrite with self implementation.
+ * Problem and solution: https://medium.com/@m-elbably/rate-limiting-the-sliding-window-algorithm-daa1d91e6196
+ * YGPT allows 1 rps, current rateLimiter allows only fixed window, cause 1s period can fails
+ */
 @Slf4j
 @Component
 public class YandexGptClient {
@@ -23,74 +33,60 @@ public class YandexGptClient {
     private final static String CLS_TASK_URI = "/foundationModels/v1/fewShotTextClassification";
     private final static String GPT_TASK_URI = "/foundationModels/v1/completion";
     private final static String CLS_TASK_DESCRIPTION = "Question classification";
-    private final static String GPT_TASK_DESCRIPTION = "Тебе дана информация о проекте и в конце вопрос к нему. Ответь на вопрос";
 
+    private final RateLimiter rateLimiter;
     private final WebClient webClient;
     private final MetadataClient metadataClient;
     private final YandexGptProperties yandexGptProperties;
+
+    @Getter
+    @AllArgsConstructor
+    public enum GptTaskDescription {
+        ANSWER_ABOUT_PROJECT("Тебе дана информация о проекте и в конце вопрос к нему. Ответь на вопрос"),
+        COMMON_ANSWER("Ты бот, который помогает студентам работающим над проектами VK Education Projects. Ответь на заданный студентом вопрос.");
+
+        private final String text;
+    }
 
     public YandexGptClient(YandexGptProperties yandexGptProperties) {
         this.webClient = WebClient.create(yandexGptProperties.host());
         this.metadataClient = new MetadataClient();
         this.yandexGptProperties = yandexGptProperties;
+        this.rateLimiter = RateLimiter.of("y-gpt-rate-limiter",
+                RateLimiterConfig.custom()
+                        .limitRefreshPeriod(Duration.ofSeconds(1))
+                        .limitForPeriod(1)
+                        .timeoutDuration(Duration.ofSeconds(30))
+                        .build());
     }
 
-    public QuestionPrediction classifyQuestion(List<String> labels, String question) throws Exception {
-        log.info("Testing request: {}", question);
+    public QuestionPrediction classifyQuestion(List<String> labels, String question) {
+        log.info("Classify question: {}", question);
         return webClient.post()
                 .uri(CLS_TASK_URI)
-                .accept(MediaType.APPLICATION_JSON)
-                .acceptCharset(StandardCharsets.UTF_8)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("Authorization", BEARER + metadataClient.getToken())
-                .bodyValue(buildRequest(labels, question))
+                .bodyValue(buildQuestionClassificationRequest(labels, question))
                 .retrieve()
                 .bodyToMono(QuestionPrediction.class)
+                .delayElement(Duration.ofSeconds(1))
+                .transformDeferred(RateLimiterOperator.of(rateLimiter))
                 .block();
     }
 
-    private QuestionPredictionRequest buildRequest(List<String> labels, String question) {
-        return new QuestionPredictionRequest(
-                CLS_URI_TEMPLATE.formatted(yandexGptProperties.folderId()),
-                CLS_TASK_DESCRIPTION,
-                labels,
-                question
-        );
-    }
-
-    public String sendQuestion(String prompt) {
-        log.info("Sending question to YandexGPT: {}", prompt);
-        ProjectQuestionGptRequest.CompletionOptions completionOptions = new ProjectQuestionGptRequest.CompletionOptions(
-                false, // stream
-                0.1,   // temperature
-                "1000" // maxTokens
-        );
-
-        List<ProjectQuestionGptRequest.Message> messages = List.of(
-                new ProjectQuestionGptRequest.Message("system", GPT_TASK_DESCRIPTION),
-                new ProjectQuestionGptRequest.Message("user", prompt)
-        );
-
-        ProjectQuestionGptRequest request = new ProjectQuestionGptRequest(
-                GPT_URI_TEMPLATE.formatted(yandexGptProperties.folderId()),
-                completionOptions,
-                messages
-        );
-
-        ProjectQuestionGptResponse response = null;
-        try {
-            response = webClient.post()
-                    .uri(GPT_TASK_URI)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .header("Authorization", BEARER + metadataClient.getToken())
-                    .bodyValue(request)
-                    .retrieve()
-                    .bodyToMono(ProjectQuestionGptResponse.class)
-                    .block();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public String generateAnswer(String prompt, GptTaskDescription taskDescription) {
+        log.info("Generate answer for promt: {}", prompt);
+        ProjectQuestionGptRequest request = buildAnswerCompletionRequest(prompt, taskDescription);
+        ProjectQuestionGptResponse response = webClient.post()
+                .uri(GPT_TASK_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", BEARER + metadataClient.getToken())
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(ProjectQuestionGptResponse.class)
+                .delayElement(Duration.ofSeconds(1))
+                .transformDeferred(RateLimiterOperator.of(rateLimiter))
+                .block();
 
         if (response == null || response.result().alternatives().isEmpty()) {
             log.error("No response received from YandexGPT");
@@ -101,4 +97,31 @@ public class YandexGptClient {
         return response.result().alternatives().getFirst().message().text();
     }
 
+    private ProjectQuestionGptRequest buildAnswerCompletionRequest(String prompt, GptTaskDescription taskDescription) {
+        ProjectQuestionGptRequest.CompletionOptions completionOptions = new ProjectQuestionGptRequest.CompletionOptions(
+                false, // stream
+                0.1,   // temperature
+                "1000" // maxTokens
+        );
+
+        List<ProjectQuestionGptRequest.Message> messages = List.of(
+                new ProjectQuestionGptRequest.Message("system", taskDescription.getText()),
+                new ProjectQuestionGptRequest.Message("user", prompt)
+        );
+
+        return new ProjectQuestionGptRequest(
+                GPT_URI_TEMPLATE.formatted(yandexGptProperties.folderId()),
+                completionOptions,
+                messages
+        );
+    }
+
+    private QuestionPredictionRequest buildQuestionClassificationRequest(List<String> labels, String question) {
+        return new QuestionPredictionRequest(
+                CLS_URI_TEMPLATE.formatted(yandexGptProperties.folderId()),
+                CLS_TASK_DESCRIPTION,
+                labels,
+                question
+        );
+    }
 }
--- src/main/java/com/vk/education_bot/handler/CommonCallbackHandler.java
+++ src/main/java/com/vk/education_bot/handler/CommonCallbackHandler.java
@@ -33,7 +33,6 @@ import java.util.regex.Pattern;
 @Component
 public class CommonCallbackHandler extends CallbackApi {
 
-    private final static String UNKNOWN_QUESTION = "Извини, я не понимаю твой вопрос";
     private final static String EMPTY = "";
 
     private final Map<Long, UserContext> userContexts = new ConcurrentHashMap<>();
@@ -107,10 +106,10 @@ public class CommonCallbackHandler extends CallbackApi {
 
     private void handleMainMenu(String userInput, UserContext context) {
         long userId = context.getUserId();
-        if ("Задать общий вопрос" .equalsIgnoreCase(userInput)) {
+        if ("Задать общий вопрос".equalsIgnoreCase(userInput)) {
             vkClient.sendMessageWithKeyboard(userId, "Введи свой вопрос", KeyboardFactory.createBackButtonKeyboard());
             context.setState(UserState.ASKING_GENERAL_QUESTION);
-        } else if ("Вопрос по проекту" .equalsIgnoreCase(userInput)) {
+        } else if ("Вопрос по проекту".equalsIgnoreCase(userInput)) {
             List<Section> sections = sectionService.getAllSections();
             vkClient.sendMessageWithKeyboard(userId, "Выбери раздел", KeyboardFactory.createSectionsKeyboard(sections));
             context.setState(UserState.SECTION_SELECTION);
@@ -214,7 +213,7 @@ public class CommonCallbackHandler extends CallbackApi {
                 userInput
         );
 
-        String response = yandexGptClient.sendQuestion(prompt);
+        String response = yandexGptClient.generateAnswer(prompt, YandexGptClient.GptTaskDescription.ANSWER_ABOUT_PROJECT);
         vkClient.sendMessage(userId, response);
         vkClient.sendMessageWithKeyboard(userId, "Ты получил ответ на свой вопрос?", KeyboardFactory.createYesNoKeyboard());
         userQuestions.put(userId, project.getName() + ": " + userInput);
@@ -232,7 +231,7 @@ public class CommonCallbackHandler extends CallbackApi {
         long userId = context.getUserId();
         String response = questionClassifier.classifyQuestion(userInput)
                 .map(Question::getAnswer)
-                .orElse(UNKNOWN_QUESTION);
+                .orElseGet(() -> yandexGptClient.generateAnswer(userInput, YandexGptClient.GptTaskDescription.COMMON_ANSWER));
 
         vkClient.sendMessage(userId, response);
         vkClient.sendMessageWithKeyboard(userId, "Ты получил ответ на свой вопрос?", KeyboardFactory.createYesNoKeyboard());
